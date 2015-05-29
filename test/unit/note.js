0a7a8a5370e6d377dcffb9e49b645901332feb3c
xyxabcdiy <xyxabcdiy@gmail.com>
2015-05-29 22:26:19 +0800

    finish model test files: test/unit/note.js  test/units/helprs.js

--- test/unit/note.js
+++ test/unit/note.js
@@ -1,3 +1,110 @@
 /**
  * Created by woaitzy on 2015/5/27 0027.
  */
+var should = require('should');
+var sinon = require('sinon');
+var proxyquire = require('proxyquire');
+var helpers = require('../units/helpers');
+var mongoose = require('mongoose');
+
+var shouldDefineSchemaProperty =
+    helpers.shouldDefineSchemaProperty.bind(null, mongoose.Schema);
+var shouldRegisterSchema = helpers.shouldRegisterSchema.bind(null,
+    mongoose.model, mongoose.Schema);
+var shouldBeRequired = helpers.shouldBeRequired.bind(null,
+    mongoose.Schema);
+var shouldBeA = helpers.shouldBeA.bind(null, mongoose.Schema);
+var shouldDefaultTo = helpers.shouldDefaultTo.bind(null,
+    mongoose.Schema);
+var shouldBeBetween = helpers.shouldBeBetween.bind(null,
+    mongoose.Schema);
+var shouldValidateThat = helpers.shouldValidateThat.bind(null,
+    mongoose.Schema);
+var shouldLoadPlugin = helpers.shouldLoadPlugin.bind(null,
+    mongoose.Schema);
+
+describe('Note', function () {
+    var Note, mongooseTimestamp;
+    before(function () {
+        mongooseTimestamp = sinon.stub();
+        Note = proxyquire('../../models/notes', {
+            'mongoose-timestamp': mongooseTimestamp,
+            'mongoose': mongoose
+        });
+    });
+
+    it('should register the Mongoose model', function () {
+        shouldRegisterSchema('Note');
+    });
+    it('should load the timestamps plugin', function () {
+        shouldLoadPlugin(mongooseTimestamp);
+    });
+
+    describe('title', function () {
+        it('should be required', function () {
+            shouldBeRequired('title');
+        });
+        it('should be string', function () {
+            shouldBeA('title', String);
+        });
+        it('should have a length of 3-255 chars', function () {
+            shouldValidateThat('title', 'isLength', 3, 255);
+        });
+    });
+
+    describe('description', function () {
+        it('should be required', function () {
+            shouldBeRequired('description');
+        });
+        it('should be string', function () {
+            shouldBeA('description', String);
+        });
+        it('should have a length of 10-255 chars', function () {
+            shouldValidateThat('description', 'isLength', 10, 255);
+        });
+    });
+    describe('userId', function () {
+        it('should be required', function () {
+            shouldBeRequired('userId');
+        });
+        it('should be userId', function () {
+            shouldBeA('userId', mongoose.Schema.ObjectID);
+        });
+        it('should be reference to the User model', function () {
+            shouldDefineSchemaProperty({userId: {ref: 'User'}});
+        });
+    });
+    describe('rating', function() {
+        it('should be a number', function() {
+            shouldBeA('rating', Number);
+        });
+
+        it('should default to 0 (unrated)', function() {
+            shouldDefaultTo('rating', 0);
+        });
+
+        it('should be between 0 and 10', function() {
+            shouldBeBetween('rating', { min: 0, max: 10 });
+        });
+    });
+
+    describe('category', function() {
+        it('should be a string', function() {
+            shouldBeA('category', String);
+        });
+
+        it('should default to general', function() {
+            shouldDefaultTo('category', 'general');
+        });
+    });
+
+    describe('public', function() {
+        it('should be a boolean', function() {
+            shouldBeA('public', Boolean);
+        });
+
+        it('should default to false', function() {
+            shouldDefaultTo('public', false);
+        });
+    });
+});
--- test/units/helpers.js
+++ test/units/helpers.js
@@ -1,3 +1,92 @@
 /**
  * Created by woaitzy on 2015/5/27 0027.
  */
+var sinon = require('sinon');
+var should = require('should');
+var validator = require('../../lib/validator');
+
+exports.getMongooseStub = function () {
+    var mongoose = {};
+    mongoose.Schema = sinon.stub();
+    mongoose.Schema.ObjectId = 'ObjectId';
+    mongoose.Schema.prototype.plugin = sinon.stub();
+    mongoose.model = sinon.stub();
+
+    return mongoose;
+};
+
+exports.shouldDefineSchemaProperty = function (Schema, property) {
+    sinon.assert.called(Schema.withArgs(sinon.match(property)));
+};
+
+exports.shouldBeRequired = function (Schema,property) {
+    var obj = {};
+    obj[property] = {
+        required: true
+    };
+    exports.shouldDefineSchemaProperty(Schema, obj);
+};
+exports.shouldBeUnique = function (Schema, property) {
+    var obj = {};
+    obj[property] = {
+        unique: true
+    };
+    exports.shouldDefineSchemaProperty(Schema, obj);
+};
+
+exports.shouldBeA = function (Schema,property,type) {
+    var obj = {};
+    obj[property] = {
+        type: type
+    };
+    exports.shouldDefineSchemaProperty(Schema, obj);
+};
+
+exports.shouldDefaultTo = function (Schema, property, defultValue) {
+    var obj = {};
+    obj[property] = {
+        defult: defultValue
+    };
+    exports.shouldDefineSchemaProperty(Schema, obj);
+};
+
+exports.shouldBeBetween = function (Schema,proprty,opts) {
+    var obj = {};
+    obj[proprty] = {
+        min: opts.min,
+        max:opts.max
+    };
+    exports.shouldDefineSchemaProperty(Schema, obj);
+};
+
+exports.shouldValidateThat = function (Schema,property) {
+    var args = Array.prototype.slice.call(arguments, 2);
+    var obj = {};
+    obj[property] = {
+        validate:validator.validate.apply(validator,args)
+    };
+    exports.shouldDefineSchemaProperty(Schema, obj);
+};
+
+exports.shouldValidateMany = function (Schema,property,validation1,validation2) {
+    var obj = {};
+    obj[property] = {
+        validate:[{
+            validator:validator.validate.apply(validator,validation1.args),
+            msg:validation1.msg
+        },{
+            validator:validator.validate.apply(validator,validation2.args),
+            msg:validation2.msg
+        }]
+    };
+    exports.shouldDefineSchemaProperty(Schema,obj);
+};
+
+exports.shouldRegisterSchema = function (Model,Schema,name) {
+    Model.calledWith(name).should.be.true;
+    Model.args[0][1].should.be.an.instanceOf(Schema);
+};
+
+exports.shouldLoadPlugin = function (Schema,plugin) {
+    sinon.assert.called(Schema.prototype.plugin.withArgs(plugin));
+};
